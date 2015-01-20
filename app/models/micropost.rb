063cd9158c18a6a3415220c8f1a9d7fe97a67286
mizutaki <mizutaki4@gmail.com>
2015-01-20 21:55:53 +0900

    To chapter 10.2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,8 @@ class Micropost < ActiveRecord::Base
 	#参照元テーブルから参照先テーブルにアクセスする Micropost → User
 	belongs_to :user
 	default_scope -> { order('created_at DESC') }
+	#contentが空じゃないかつ140文字を超えない
+	validates :content, presence: true, length: { maximum: 140 }
 	#user_idが空でないか
 	validates :user_id, presence: true
 end
